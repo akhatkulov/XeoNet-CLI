4453e5f57cfae1445a3a4ce59ddc100ad900bcaf
Mekhroj Akhatkulov <151901779+akhatkulov@users.noreply.github.com>
2024-06-17 10:03:23 +0300

    build 0.3

--- app.py
+++ app.py
@@ -26,13 +26,13 @@ def main(status: Optional[str] = typer.Argument("home")):
       ]
       y = inquirer.prompt(lang)
       if "UZBEK" in y["lang"]:
-        tm  = typer.prompt(f"{typer.style('How often do you want your address changed?',fg=typer.colors.YELLOW)}")
+        tm  = typer.prompt(f"{typer.style('Manzilingizni qanchalik tez-tez almashishini xohlaysiz?(soniya)',fg=typer.colors.YELLOW)}")
         main_changer(x=int(tm))
       if "RUSSIAN" in y["lang"]:
-        tm  = typer.prompt(f"{typer.style('Как часто вы хотите, чтобы ваш адрес менялся?')}")
+        tm  = typer.prompt(f"{typer.style('Как часто вы хотите, чтобы ваш адрес менялся? (секунды)',fg=typer.colors.YELLOW)}")
         main_changer(x=int(tm))
       if "ENGLISH" in y["lang"]:
-        tm  = typer.prompt(f"{typer.style('How often do you want your address changed?')}")
+        tm  = typer.prompt(f"{typer.style('How often do you want your address changed? (seconds)',fg=typer.colors.YELLOW)}")
         main_changer(x=int(tm))
 
 if __name__ == "__main__":
